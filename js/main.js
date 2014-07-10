4048cad78155f45a0dd3903693f11ab451ea6158
Ronald Victorino <ron@nthgensoftware.com>
2014-07-10 15:17:13 -0400

    Resized images, edited JS files to animate navbar, pagination on slider works now.

--- js/main.js
+++ js/main.js
@@ -1,11 +1,24 @@
 $(function(){
 
+	// Navbar Scroll Animations
+	$('#navbar ul li').click(function() {
+		var $section = $('section');
+		var currentLink = $(this).attr('data-sectionNumber');
+		$('body').stop().animate( { scrollTop : $section.eq( currentLink ).offset().top }, 650, 'easeInOutExpo');
+	});
+
 	$('.main-page-text').css('opacity', '1');
 	$('.main-page-text').addClass('fadeInUp');
 
 	// Image Slider for Wedding Photos
-	$('.jcarousel').jcarousel();
 
+	// jCarousel initialization
+	$('.jcarousel').jcarousel({
+		wrap: 'both',
+		transforms3d:true
+	});
+
+	// jCarousel arrow sliders
 	$('#left-slider-arrow').click(function() {
 		$('.jcarousel').jcarousel('scroll', '-=1');
 	});
@@ -13,5 +26,12 @@ $(function(){
 	$('#right-slider-arrow').click(function() {
 		$('.jcarousel').jcarousel('scroll', '+=1');
 	});
+
+	// jCarousel pagination
+	$('.jcarousel-pagination').on('jcarouselpagination:active', 'a', function() {
+      $(this).addClass('active');
+  }).on('jcarouselpagination:inactive', 'a', function() {
+      $(this).removeClass('active');
+  }).jcarouselPagination();
 
 });
