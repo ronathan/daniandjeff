a2efcdbf1202012a31427e4af03f08223337daac
Ronald Victorino <ron@nthgensoftware.com>
2014-07-10 12:34:44 -0400

    Added slider images.

--- js/main.js
+++ js/main.js
@@ -1,4 +1,17 @@
 $(function(){
+
 	$('.main-page-text').css('opacity', '1');
 	$('.main-page-text').addClass('fadeInUp');
+
+	// Image Slider for Wedding Photos
+	$('.jcarousel').jcarousel();
+
+	$('#left-slider-arrow').click(function() {
+		$('.jcarousel').jcarousel('scroll', '-=1');
+	});
+
+	$('#right-slider-arrow').click(function() {
+		$('.jcarousel').jcarousel('scroll', '+=1');
+	});
+
 });
